936b5bdf3d5485e34d007cdb888fec47d0909ea2
Thomas Proisl <thomas.proisl@fau.de>
2018-03-06 09:48:25 +0100

    Bump version number

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ here = path.abspath(path.dirname(__file__))
 with open(path.join(here, 'README.rst')) as fh:
     long_description = fh.read()
 
-version = "0.1.1"
+version = "0.1.2"
 
 setup(
     name='CorpConv',
